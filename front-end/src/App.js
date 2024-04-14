a1ef6775426b51c9211c1f87c3ba38ccdfb8dbf1
byrybdyk <byrybdyk.byrybdyk@gmail.com>
2024-04-14 15:46:07 +0300

    lb3 front done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Home_page';
 import LB2Page from './lb2/lab2';
+import LB3Page from './lb3/lab3';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/lb2" element={<LB2Page/>}/>
+                    <Route path="/lb3" element={<LB3Page/>}/>
                 </Routes>
             </div>
             
--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  let currentURL = window.location.href;
+  let pageName ="";
+  if ((currentURL === 'http://localhost:3000/lb2')||(currentURL === 'http://dr-chainsaw.ru/lb2')){
+    pageName = "Lab 2";
+  }
+  if ((currentURL === 'http://localhost:3000/lb3')||(currentURL === 'http://dr-chainsaw.ru/lb3')){
+    pageName = "Lab 3";
+  }
   return (
+    
     <header className="header">
         
         <div class="header-container">
             <a className="main-header-item" href="/" target="_blank"> <h2>Computational math</h2></a>
-            <a className="header-item" href="/lb2" target="_blank"> <h2>Lab 2</h2></a>
+            <a className="header-item" href={currentURL} target="_blank"> <h2>{pageName}</h2></a>
             <a className="header-item"  target="_blank"> <h2>Zarubov Egor</h2></a>
             <a className="header-item" target="_blank"> <h2>P3218</h2></a>
         </div>
