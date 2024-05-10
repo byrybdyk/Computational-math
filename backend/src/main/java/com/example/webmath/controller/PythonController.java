df9ac641bf464625f32afcd21a8e021a49976f52
byrybdyk <byrybdyk@gmail.com>
2024-05-10 19:21:55 +0300

    add History task

--- backend/src/main/java/com/example/webmath/controller/PythonController.java
+++ backend/src/main/java/com/example/webmath/controller/PythonController.java
@@ -14,7 +14,7 @@ import java.io.*;
 import java.util.List;
 
 @RestController
-// public class PythonController {
+public class PythonController {
 
     @GetMapping("api/run-python-script")
     public String runPythonScript(@RequestParam("lbname") String lbname, @RequestParam("type") String type,
